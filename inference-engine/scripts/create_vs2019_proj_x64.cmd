9d3d3628d10755aa2e5732288233564e78000283
Mike Deisher <michael.deisher@intel.com>
2020-08-06 09:13:10 -0700

    VS 2019 build script from Pavel -- faster than 2017

---
+++ inference-engine/scripts/create_vs2019_proj_x64.cmd
@@ -0,0 +1,54 @@
+@echo off
+
+pushd ..\..
+if not exist "vs2019x64Debug" (
+               mkdir "vs2019x64withDebug"
+)
+if not exist "vs2019x64Release" (
+               mkdir "vs2019x64withRelease"
+)
+
+cmake -E chdir "vs2019x64withDebug" cmake -G "Visual Studio 16 2019" -A x64^
+    -DCMAKE_BUILD_TYPE=Debug ^
+    -DOS_FOLDER=ON ^
+    -DENABLE_MYRIAD=OFF ^
+    -DENABLE_VPU=OFF ^
+    -DENABLE_GNA=ON ^
+    -DENABLE_CLDNN=OFF ^
+    -DENABLE_TESTS=ON ^
+    -DENABLE_BEH_TESTS=ON ^
+    -DENABLE_FUNCTIONAL_TESTS=ON ^
+    -DENABLE_PYTHON=OFF ^
+    -DENABLE_OPENCV=ON ^
+    -DENABLE_MKL_DNN=ON ^
+    -DVERBOSE_BUILD=OFF ^
+    -DENABLE_MODELS=OFF ^
+    -DENABLE_VALIDATION_SET=OFF ^
+    -DENABLE_PRIVATE_MODELS=OFF ^
+    -DENABLE_CLANG_FORMAT=OFF ^
+    -DGNA_LIBRARY_VERSION=GNA2 ^
+    ..
+
+cmake -E chdir "vs2019x64withRelease" cmake -G "Visual Studio 16 2019" -A x64^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DOS_FOLDER=ON ^
+    -DENABLE_MYRIAD=OFF ^
+    -DENABLE_VPU=OFF ^
+    -DENABLE_GNA=ON ^
+    -DENABLE_CLDNN=OFF ^
+    -DENABLE_TESTS=ON ^
+    -DENABLE_BEH_TESTS=ON ^
+    -DENABLE_FUNCTIONAL_TESTS=ON ^
+    -DENABLE_PYTHON=OFF ^
+    -DENABLE_OPENCV=ON ^
+    -DENABLE_MKL_DNN=ON ^
+    -DVERBOSE_BUILD=OFF ^
+    -DENABLE_MODELS=OFF ^
+    -DENABLE_VALIDATION_SET=OFF ^
+    -DENABLE_PRIVATE_MODELS=OFF ^
+    -DENABLE_CLANG_FORMAT=OFF ^
+    -DGNA_LIBRARY_VERSION=GNA2 ^
+    ..
+
+popd
+pause
